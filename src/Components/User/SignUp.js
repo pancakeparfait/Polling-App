09f101b7afb55569e729f77740def055a86b0492
Mansa Samlafo <msamlafo@gmail.com>
2021-07-09 14:14:30 -0400

    Remove my awesomeness.

--- src/Components/User/SignUp.js
+++ src/Components/User/SignUp.js
@@ -15,44 +15,68 @@ class SignUp extends Component {
     };
   }
   render() {
-      return(
+    return (
       <React.Fragment>
-          <p className="text-center">
-          Polling App
-        </p>
-          <h4 className="sub-title">
-              Register
-          </h4>
-          <br />
-          <form className="abxyzeeezee">
+        <p className="text-center">Polling App</p>
+        <h4 className="sub-title">Register</h4>
+        <br />
+        <form>
           <FormGroup>
-              <Input type="email" name="email" id="exampleEmail" placeholder="Name" className="mb-2" />
-            </FormGroup>
-
-            <FormGroup>
-              <Input type="email" name="email" id="exampleEmail" placeholder="Email" className="mb-2" />
-            </FormGroup>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="Name"
+              className="mb-2"
+            />
+          </FormGroup>
 
           <FormGroup>
-              <Input type="email" name="email" id="exampleEmail" placeholder="Password" className="mb-2" />
-            </FormGroup>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="Email"
+              className="mb-2"
+            />
+          </FormGroup>
 
-            <FormGroup>
-              <Input type="email" name="email" id="exampleEmail" placeholder="Confirm Password" className="mb-5" />
-            </FormGroup>
+          <FormGroup>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="Password"
+              className="mb-2"
+            />
+          </FormGroup>
 
-            <div className="container">
-              <div className="row">
-                <div className="col text-center">
-                  <Button className="btn btn-default btn-lg btn-submit mb-5">Submit</Button>
-                  <br />
-                  <p Link="/SignIn" className="small-text">Back to Sign In</p>
-                </div>
+          <FormGroup>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="Confirm Password"
+              className="mb-5"
+            />
+          </FormGroup>
+
+          <div className="container">
+            <div className="row">
+              <div className="col text-center">
+                <Button className="btn btn-default btn-lg btn-submit mb-5">
+                  Submit
+                </Button>
+                <br />
+                <p Link="/SignIn" className="small-text">
+                  Back to Sign In
+                </p>
               </div>
             </div>
-          </form>
+          </div>
+        </form>
       </React.Fragment>
-      )
+    );
   }
 }
 
